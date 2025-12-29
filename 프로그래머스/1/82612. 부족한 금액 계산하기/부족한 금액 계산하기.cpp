93b853a8d295fc4cd562ff354d950f53dbce61b3
Ho Seung <wjdghtmd1238@gmail.com>
2025-12-29 10:01:00 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.03 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = 0;
+    for(int i=1;i<=count;i++)
+    {
+       answer += price*i;
+    }
+    if(answer >= money) {
+        answer = answer -money;
+        return answer;
+    }
+    else{return 0;}
+
+}
