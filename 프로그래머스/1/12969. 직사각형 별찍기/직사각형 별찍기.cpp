f8988c184110a4451dd2df5417bdab9d06c0ccfa
Ho Seung <wjdghtmd1238@gmail.com>
2025-12-29 18:49:25 +0900

    [level 1] Title: 직사각형 별찍기, Time: 4.82 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    for(int i=1; i<=b; i++)
+    {
+       
+        for(int j=1; j<=a;j++)
+        {
+           
+            cout << "*";
+        }
+        
+        cout<<endl;
+      
+        
+    }
+    return 0;
+}
